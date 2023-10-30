9a1f49341aa6fa41097bf4366efb519ac2ac6493
Taha <98838967+Taha-Hassan-Git@users.noreply.github.com>
2023-10-30 16:34:07 +0000

    remove comments

--- src/SpeechBubble/ui-overrides.ts
+++ src/SpeechBubble/ui-overrides.ts
@@ -5,11 +5,8 @@ import {
   toolbarItem,
 } from "@tldraw/tldraw";
 
-// In order to see select our custom shape tool, we need to add it to the ui.
-
 export const uiOverrides: TLUiOverrides = {
   tools(editor, tools) {
-    // Create a tool item in the ui's context.
     tools.speech = {
       id: "speech-bubble",
       icon: "color",
@@ -23,12 +20,10 @@ export const uiOverrides: TLUiOverrides = {
     return tools;
   },
   toolbar(_app, toolbar, { tools }) {
-    // Add the tool item from the context to the toolbar.
     toolbar.splice(4, 0, toolbarItem(tools.speech));
     return toolbar;
   },
   keyboardShortcutsMenu(_app, keyboardShortcutsMenu, { tools }) {
-    // Add the tool item from the context to the keyboard shortcuts dialog.
     const toolsGroup = keyboardShortcutsMenu.find(
       (group) => group.id === "shortcuts-dialog.tools"
     ) as TLUiMenuGroup;
