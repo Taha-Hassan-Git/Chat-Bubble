9345dacde10ec8b989c06b3b2b3a6ff3b0e1b182
Taha <98838967+Taha-Hassan-Git@users.noreply.github.com>
2023-12-06 17:20:54 +0000

    implement 360 tail movement

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,31 @@
-import './App.css'
-import { Editor, Tldraw } from '@tldraw/tldraw'
-import '@tldraw/tldraw/tldraw.css'
-import { SpeechBubbleUtil } from './SpeechBubble/SpeechBubble'
-import { snapshot } from './data'
-import { SpeechBubbleTool } from './SpeechBubble/SpeechBubbleTool'
-import { uiOverrides } from './SpeechBubble/ui-overrides'
-import { useCallback } from 'react'
+import "./App.css";
+import { Editor, Tldraw } from "@tldraw/tldraw";
+import "@tldraw/tldraw/tldraw.css";
+import { SpeechBubbleUtil } from "./SpeechBubble/SpeechBubble";
+import { SpeechBubbleTool } from "./SpeechBubble/SpeechBubbleTool";
+import { uiOverrides } from "./SpeechBubble/ui-overrides";
+import { useCallback } from "react";
 
-const shapeUtils = [SpeechBubbleUtil]
-const tools = [SpeechBubbleTool]
+const shapeUtils = [SpeechBubbleUtil];
+const tools = [SpeechBubbleTool];
 
 function App() {
-	const handleMount = useCallback((editor: Editor) => {
-		// editor.store.loadSnapshot(snapshot)
-		// editor.setCamera({ x: 0, y: 0, z: 1 })
-	}, [])
+  const handleMount = useCallback((editor: Editor) => {
+    // editor.store.loadSnapshot(snapshot)
+    // editor.setCamera({ x: 0, y: 0, z: 1 })
+  }, []);
 
-	return (
-		<div style={{ position: 'fixed', inset: 0 }}>
-			<Tldraw
-				shapeUtils={shapeUtils}
-				tools={tools}
-				overrides={uiOverrides}
-				persistenceKey="whatever"
-				// onMount={handleMount}
-			/>
-		</div>
-	)
+  return (
+    <div style={{ position: "fixed", inset: 0 }}>
+      <Tldraw
+        shapeUtils={shapeUtils}
+        tools={tools}
+        overrides={uiOverrides}
+        persistenceKey="whatever"
+        // onMount={handleMount}
+      />
+    </div>
+  );
 }
 
-export default App
+export default App;
--- src/SpeechBubble/SpeechBubble.tsx
+++ src/SpeechBubble/SpeechBubble.tsx
@@ -54,31 +54,12 @@ export class SpeechBubbleUtil extends ShapeUtil<SpeechBubbleShape> {
   }
 
   getGeometry(shape: SpeechBubbleShape): Geometry2d {
-    const {
-      w,
-      h,
-      handles: { handle },
-    } = shape.props;
-    const { intersection, offset } = getHandleIntersectionPoint({
-      w,
-      h,
-      handle,
+    const speechBubbleGeometry = getSpeechBubbleGeometry(shape);
+    const body = new Polygon2d({
+      points: speechBubbleGeometry,
+      isFilled: true,
     });
-    if (intersection) {
-      const body = new Polygon2d({
-        points: [
-          new Vec2d(handle.x, handle.y),
-          new Vec2d(intersection.x - offset, intersection.y),
-          new Vec2d(0, h),
-          new Vec2d(0, 0),
-          new Vec2d(w, 0),
-          new Vec2d(w, h),
-          new Vec2d(intersection.x + offset, intersection.y),
-        ],
-        isFilled: true,
-      });
-      return body;
-    }
+    return body;
   }
 
   override getHandles(shape: SpeechBubbleShape) {
@@ -95,7 +76,20 @@ export class SpeechBubbleUtil extends ShapeUtil<SpeechBubbleShape> {
       h: next.props.h,
       handle: next.props.handles.handle,
     });
-    if (!intersection) return _;
+    if (!intersection)
+      return {
+        ...next,
+        props: {
+          ...next.props,
+          handles: {
+            ...next.props.handles,
+            handle: {
+              ...next.props.handles.handle,
+              y: _.props.handles.handle.y,
+            },
+          },
+        },
+      };
     const intersectionVector = new Vec2d(intersection.x, intersection.y);
     const handleVector = new Vec2d(
       next.props.handles.handle.x,
@@ -112,10 +106,9 @@ export class SpeechBubbleUtil extends ShapeUtil<SpeechBubbleShape> {
         handleVector.y - intersectionVector.y,
         handleVector.x - intersectionVector.x
       );
-      console.log(angle);
+
       const direction = Vec2d.FromAngle(angle, MIN_DISTANCE);
       newPoint = intersectionVector.add(direction);
-      console.log(newPoint);
     }
 
     return {
@@ -150,18 +143,21 @@ export class SpeechBubbleUtil extends ShapeUtil<SpeechBubbleShape> {
   };
 
   component(shape: SpeechBubbleShape) {
-    const d = getSpeechBubblePath(shape);
+    const geometry = getSpeechBubbleGeometry(shape);
+    const pathData = "M" + geometry[0] + "L" + geometry.slice(1) + "Z";
     return (
       <>
         <svg className="tl-svg-container">
-          <path d={d} stroke={"black"} strokeWidth={2} fill="none" />
+          <path d={pathData} stroke={"black"} strokeWidth={2} fill="none" />
         </svg>
       </>
     );
   }
 
   indicator(shape: SpeechBubbleShape) {
-    return <path d={getSpeechBubblePath(shape)} />;
+    const geometry = getSpeechBubbleGeometry(shape);
+    const pathData = "M" + geometry[0] + "L" + geometry.slice(1) + "Z";
+    return <path d={pathData} />;
   }
 
   override onResize: TLOnResizeHandler<SpeechBubbleShape> = (shape, info) => {
@@ -214,6 +210,7 @@ function getHandleIntersectionPoint({
   ];
 
   const intersection = intersectLineSegmentPolygon(handleVec, center, box);
+
   // A ___I__ M _______ B
   const a = new Vec2d(w, h);
   const b = new Vec2d(0, h);
@@ -221,29 +218,90 @@ function getHandleIntersectionPoint({
 
   // lerp
   if (intersection) {
-    return { intersection: intersection[0], offset };
+    // lines
+    ///      0
+    //  _____________
+    //  |           |
+    // 3|           | 1
+    //  |           |
+    //  -------------
+    //      2
+
+    let line: 0 | 1 | 2 | 3;
+    console.log({ x: intersection[0].x, y: intersection[0].y });
+    if (Math.round(intersection[0].y) === h) line = 2;
+    if (Math.round(intersection[0].y) < 4) line = 0;
+    if (Math.round(intersection[0].x) === w) line = 1;
+    if (Math.round(intersection[0].x) < 4) line = 3;
+    console.log(line);
+    return { intersection: intersection[0], offset, line };
   }
   return { intersection: null, offset };
 }
 
-export function getSpeechBubblePath(shape: SpeechBubbleShape) {
-  const {
-    w,
-    h,
-    handles: { handle },
-  } = shape.props;
-  const { intersection, offset } = getHandleIntersectionPoint({ w, h, handle });
-  if (intersection) {
-    const d = `
-            M${handle.x},${handle.y}
-            L${intersection.x - offset},${intersection.y}
-            L-${0},${h}
-            L${0},${0}
-            L${w},${0}
-            L${w},${h}
-            L${intersection.x + offset},${intersection.y}
-            z`;
+const getSpeechBubbleGeometry = (shape: SpeechBubbleShape): Vec2d[] => {
+  const { intersection, offset, line } = getHandleIntersectionPoint({
+    w: shape.props.w,
+    h: shape.props.h,
+    handle: shape.props.handles.handle,
+  });
 
-    return d;
+  const handle = shape.props.handles.handle;
+
+  const initialSegments = [
+    new Vec2d(0, 0),
+    new Vec2d(shape.props.w, 0),
+    new Vec2d(shape.props.w, shape.props.h),
+    new Vec2d(0, shape.props.h),
+  ];
+
+  if (!intersection) {
+    return initialSegments;
   }
-}
+
+  const createTailSegments = (orientation: "horizontal" | "vertical") => {
+    return orientation === "horizontal"
+      ? [
+          line === 0
+            ? new Vec2d(intersection.x - offset, intersection.y)
+            : new Vec2d(intersection.x + offset, intersection.y),
+          new Vec2d(handle.x, handle.y),
+          line === 0
+            ? new Vec2d(intersection.x + offset, intersection.y)
+            : new Vec2d(intersection.x - offset, intersection.y),
+        ]
+      : [
+          line === 1
+            ? new Vec2d(intersection.x, intersection.y - offset)
+            : new Vec2d(intersection.x, intersection.y + offset),
+          new Vec2d(handle.x, handle.y),
+          line === 1
+            ? new Vec2d(intersection.x, intersection.y + offset)
+            : new Vec2d(intersection.x, intersection.y - offset),
+        ];
+  };
+
+  let modifiedSegments = [...initialSegments];
+
+  switch (line) {
+    case 0:
+      modifiedSegments.splice(1, 0, ...createTailSegments("horizontal"));
+      break;
+    case 1:
+      modifiedSegments.splice(2, 0, ...createTailSegments("vertical"));
+      break;
+    case 2:
+      modifiedSegments.splice(3, 0, ...createTailSegments("horizontal"));
+      break;
+    case 3:
+      modifiedSegments = [
+        ...modifiedSegments,
+        ...createTailSegments("vertical"),
+      ];
+      break;
+    default:
+      throw new Error("Invalid line number");
+  }
+
+  return modifiedSegments;
+};
